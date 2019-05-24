0bd377973f7867c6e33d310f7ded3c13b2394592
leehyeongrak <leehrak@gmail.com>
2019-05-24 15:43:51 +0900

    앱아이콘, 탭바아이콘 추가 및 디자인 수정

--- DoneMeals/TabBarController.swift
+++ DoneMeals/TabBarController.swift
@@ -16,6 +16,14 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
+        
+        let tabBarItems = tabBar.items! as [UITabBarItem]
+        tabBarItems[2].title = nil
+        tabBarItems[2].imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        UITabBar.appearance().layer.borderWidth = 0.0
+        UITabBar.appearance().clipsToBounds = true
+        
         // Do any additional setup after loading the view.
     }
     
